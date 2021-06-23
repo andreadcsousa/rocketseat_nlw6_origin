b8db538645e576e00d21a10bdaeffbe6ba41f690
Andrea Sousa <77935458+andreadcsousa@users.noreply.github.com>
2021-06-23 15:34:08 -0300

    atualização do projeto

--- js/script.js
+++ js/script.js
@@ -1,32 +1,61 @@
 // abre e fecha o menu quando clicar nos ícones: hamburger e x
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show');
-    })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
 // quando clicar em um item do menu, esconder menu
-const links = document.querySelectorAll('nav ul li a');
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show');
-    })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
 // mudar o header da página quando der scroll
-const header = document.querySelector('#header');
+const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= navHeight) {
-        // scroll é maior que a altura do header
-        header.classList.add('scroll');
-    } else {
-        // menor que a altura do header
-        header.classList.remove('scroll');
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
+
+// testimonials carousel slider swiper
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mouseWheel: true,
+  keyboard: true,
+});
+
+// mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    #footer .text, #footer .social
+    `,
+  { interval: 100 }
+);
