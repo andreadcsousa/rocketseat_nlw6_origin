787a75b04bebb1d628d66d568863df4bab7923d9
Andrea Sousa <77935458+andreadcsousa@users.noreply.github.com>
2021-06-25 05:12:33 -0300

    ultima aula

--- js/script.js
+++ js/script.js
@@ -39,6 +39,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mouseWheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 // mostrar elementos quando der scroll na página
@@ -71,8 +77,34 @@ function backToTop() {
   }
 }
 
+// menu ativo conforme a seção fica visível na página
+const sections = document.querySelectorAll("main section[id]");
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
 // quando rolar a página, executa
 window.addEventListener("scroll", function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
